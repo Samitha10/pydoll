5a9dcaa8747a301eeca8abf958f8b5248bd1a866
Samitha10 <samithabesinghe1999@gmail.com>
2026-01-04 10:29:35 +0530

    Change misspelled word Brave Add the browser call  to __init__

--- pydoll/browser/chromium/__init__.py
+++ pydoll/browser/chromium/__init__.py
@@ -1,7 +1,9 @@
+from pydoll.browser.chromium.brave import Brave
 from pydoll.browser.chromium.chrome import Chrome
 from pydoll.browser.chromium.edge import Edge
 
 __all__ = [
     'Edge',
     'Chrome',
+    'Brave',
 ]
